72cc2b36aaa4e78d8cac044704ab9950723af3e0
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-11-27 12:50:32 +0000

    adding chrome types, updating manifest to use new files, background and contentScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "index.htm"),
+        background: resolve(__dirname, "src/background.ts"),
+        contentScript: resolve(__dirname, "src/contentScript.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
