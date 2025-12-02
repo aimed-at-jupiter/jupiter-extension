8324f189820b6120c066c5b1f70ef5cacfe7c213
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-12-02 16:45:09 +0000

    refactoring contentScript

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,20 +1,23 @@
 import type { FillFormMessage } from "./types";
 
+function setInputValue(id: string, value?: string) {
+  const input = document.getElementById(id) as HTMLInputElement | null;
+  if (!input) {
+    console.log(`Input with id "${id}" not found`);
+    return;
+  }
+  if (value !== undefined) input.value = value;
+}
+
 chrome.runtime.onMessage.addListener((msg: FillFormMessage) => {
   if (msg.type !== "FILL_FORM") return;
 
   const clientData = msg.payload;
-
-  (document.getElementById("full-name") as HTMLInputElement).value =
-    clientData.fullName;
-  (document.getElementById("email") as HTMLInputElement).value =
-    clientData.email;
-  (document.getElementById("address-line-1") as HTMLInputElement).value =
-    clientData.address.line1;
-  (document.getElementById("city") as HTMLInputElement).value =
-    clientData.address.city;
-  (document.getElementById("post-code") as HTMLInputElement).value =
-    clientData.address.postCode;
-  (document.getElementById("phone") as HTMLInputElement).value =
-    clientData.phone;
+  setInputValue("title", clientData.title);
+  setInputValue("full-name", clientData.fullName);
+  setInputValue("email", clientData.email);
+  setInputValue("address-line-1", clientData.address.line1);
+  setInputValue("city", clientData.address.city);
+  setInputValue("post-code", clientData.address.postCode);
+  setInputValue("phone", clientData.phone);
 });
