2d23ef06652adfcb83fd0c79b625a2a8a8590e7a
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-12-02 16:10:46 +0000

    moving script to server, updating sql

--- server/db/setup-db.sql
+++ server/db/setup-db.sql
@@ -1,2 +1,16 @@
 DROP DATABASE IF EXISTS jupiter_clients;
 CREATE DATABASE jupiter_clients;
+
+\connect jupiter_clients
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(5),
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    address_line1 TEXT,
+    city TEXT,
+    post_code TEXT
+);
